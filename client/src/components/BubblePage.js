2253a3f8f1f76f873008362124ffdb590b68714a
Gabriellia Venegas-Johnson <61035529+venegasgabby76@users.noreply.github.com>
2020-09-11 16:56:03 -0700

    got some stuff written out but my data wont show up

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -3,11 +3,24 @@ import axios from "axios";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
+import { axiosWithAuth } from "../util/axiosWithAuth";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+    const grabColors = () => {
+      axiosWithAuth()
+      .get('http://localhost:5000/api/colors')
+      .then(response => {
+        setColorList(response.data);
+      })
+      .catch(error => console.log(error))
+    }
+    grabColors();
+  }, []);
 
   return (
     <>
